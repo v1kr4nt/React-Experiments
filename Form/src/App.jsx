ba47f6e7b77f4197e4e01d813bf335c36d1f50b9
vikrant <notvikrant80@gmail.com>
2024-01-15 23:08:54 +0530

    no message

--- Form/src/App.jsx
+++ Form/src/App.jsx
@@ -1,17 +1,35 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { FormInput } from "./components/FormInput";
 
 function App() {
+  const [username, setUsername] = useState("");
+  console.log(username);
   return (
     <div className="app">
-      <form>
-        <FormInput placeholder="Username" />
+      {/* <form>
+        <FormInput placeholder="Username" setUsername={setUsername} />
         <FormInput placeholder="Email" />
-        <FormInput />
-        <FormInput />
-      </form>
+        <FormInput placeholder="abc" />
+        <FormInput placeholder="asd" />
+      </form> */}
+      <Testing />
     </div>
   );
 }
 
 export default App;
+
+function Testing() {
+  const [price, setPrice] = useState("$35k");
+  const divRef = useRef();
+
+  useEffect(() => {
+    setInterval(() => {
+      divRef.current.innerHTML = "The cost of this car is $40k";
+    }, 4000);
+  }, []);
+
+  console.log("re-render");
+  return <div ref={divRef}>The cost of this car is {price}</div>;
+}
--- Form/src/components/FormInput.jsx
+++ Form/src/components/FormInput.jsx
@@ -1,10 +1,10 @@
 import "./formInput.css";
 
-export function FormInput() {
+export function FormInput(props) {
   return (
     <div className="formInout">
-      <label>Username</label>
-      <input />
+      {/* <label>Username</label> */}
+      <input placeholder={props.placeholder} onChange={e=>props.setUsername(e.target.value)} />
     </div>
   );
 }
